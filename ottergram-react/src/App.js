639af3e9c1f03dc66c3b55775ad594159222ebc8
Tim Perez <timperez624@gmail.com>
2024-06-22 12:12:45 -0700

    otter images

--- ottergram-react/src/App.js
+++ ottergram-react/src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import Barry from './otters/otter1.jpg';
+import Robin from './otters/otter2.jpg';
+import Maurice from './otters/otter3.jpg';
+import Lesley from './otters/otter4.jpg';
+import Barbara from './otters/otter5.jpg';
 
 function App() {
   return (
@@ -6,6 +11,38 @@ function App() {
       <header>
         <h1>Ottergram</h1>
       </header>
+      <ul>
+        <li>
+          <button>
+            <img src={Barry} alt='Barry'/>
+            <p>Barry</p>
+          </button>
+        </li>
+        <li>
+          <button>
+            <img src={Robin} alt='Robin'/>
+            <p>Robin</p>
+          </button>
+        </li>
+        <li>
+          <button>
+            <img src={Maurice} alt='Maurice'/>
+            <p>Maurice</p>
+          </button>
+        </li>
+        <li>
+          <button>
+            <img src={Lesley} alt='Lesley'/>
+            <p>Lesley</p>
+          </button>
+        </li>
+        <li>
+          <button>
+            <img src={Barbara} alt='Barbara'/>
+            <p>Barbara</p>
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
