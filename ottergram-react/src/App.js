d8eaaa5ab5f2765ae71eff2fa7854cd5c05f885d
Tim Perez <timperez624@gmail.com>
2024-06-22 15:08:42 -0700

    chapter 2

--- ottergram-react/src/App.js
+++ ottergram-react/src/App.js
@@ -1,48 +1,38 @@
 import './App.css';
+import Header from './components/Header';
+import Post from './components/Post';
+import Footer from './components/Footer';
+
 import Barry from './otters/otter1.jpg';
 import Robin from './otters/otter2.jpg';
 import Maurice from './otters/otter3.jpg';
 import Lesley from './otters/otter4.jpg';
 import Barbara from './otters/otter5.jpg';
 
+const ottersArray = [
+  {image: Barry, name: 'Barry', id: 1},
+  {image: Robin, name: 'Robin', id: 2},
+  {image: Maurice, name: 'Maurice', id: 3},
+  {image: Lesley, name: 'Lesley', id: 4},
+  {image: Barbara, name: 'Barbara', id: 5},
+];
+
 function App() {
   return (
     <div>
-      <header>
-        <h1>Ottergram</h1>
-      </header>
-      <ul>
-        <li>
-          <button>
-            <img src={Barry} alt='Barry'/>
-            <p>Barry</p>
-          </button>
-        </li>
-        <li>
-          <button>
-            <img src={Robin} alt='Robin'/>
-            <p>Robin</p>
-          </button>
-        </li>
-        <li>
-          <button>
-            <img src={Maurice} alt='Maurice'/>
-            <p>Maurice</p>
-          </button>
-        </li>
-        <li>
-          <button>
-            <img src={Lesley} alt='Lesley'/>
-            <p>Lesley</p>
-          </button>
-        </li>
-        <li>
-          <button>
-            <img src={Barbara} alt='Barbara'/>
-            <p>Barbara</p>
-          </button>
-        </li>
+      <Header />
+      <ul className='post-list'>
+        {ottersArray.map((post) => (
+          <Post 
+          key={post.id}
+          image={post.image}
+          name={post.name}
+          />
+        ))}
       </ul>
+      <Footer 
+       author='Tim Perez'
+      />
     </div>
   );
 }
---
+++ ottergram-react/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer({ author }) {
+  return (
+    <footer>
+      <p>Build by: {author}</p>
+    </footer>
+  );
+}
+
+export default Footer;
