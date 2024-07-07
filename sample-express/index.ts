7173bb1185dad9c64a1692a89a973f992b418ef9
Tim Perez <timperez624@gmail.com>
2024-07-07 02:02:27 -0700

    exercise 3

---
+++ sample-express/index.ts
@@ -0,0 +1,34 @@
+import {routeHello, routeAPINames, routeWeather} from "./routes.js";
+import express, {Request, Response} from "express";
+
+const server = express();
+const port = 3000;
+
+server.get("/hello", function (_req: Request, res: Response): void {
+    const response = routeHello();
+    res.send(response);
+});
+
+server.get("/api/names",
+    async function (_req: Request, res: Response): Promise<void> {
+        let response: string;
+        try {
+            response = await routeAPINames();
+            res.send(response);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+);
+
+server.get(
+    "/api/weather/:zipcode",
+    function (req: Request, res: Response): void {
+        const response = routeWeather({zipcode: req.params.zipcode});
+        res.send(response);
+    }
+);
+
+server.listen(port, function (): void {
+    console.log("Listening on " + port);
+});
