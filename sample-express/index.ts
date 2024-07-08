967eaf5b3badac8e0c4f937cf1f04aa98213695f
Tim Perez <timperez624@gmail.com>
2024-07-07 20:42:50 -0700

    chapter 4 complete developer

--- sample-express/index.ts
+++ sample-express/index.ts
@@ -1,6 +1,8 @@
 import {routeHello, routeAPINames, routeWeather} from "./routes.js";
 import express, {Request, Response} from "express";
 
+import path from "path";
+
 const server = express();
 const port = 3000;
 
@@ -28,6 +30,12 @@ server.get(
         res.send(response);
     }
 );
+
+server.get("/components/weather", function (req: Request, res: Response): void {
+    const filePath = path.join(process.cwd(), "public", "weather.html");
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(filePath);
+});
 
 server.listen(port, function (): void {
     console.log("Listening on " + port);
