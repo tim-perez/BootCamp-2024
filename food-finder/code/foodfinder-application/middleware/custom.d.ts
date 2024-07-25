6c6977fa2b8f49e84641cf617d051b33f26de49b
Tim Perez <timperez624@gmail.com>
2024-07-24 21:11:21 -0700

    delete food finder

--- food-finder/code/foodfinder-application/middleware/custom.d.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-declare global {
-    var mongoose: mongoose;
-}
--- food-finder/code/foodfinder-application/mongoose/locations/model.ts
+++
@@ -1,5 +0,0 @@
-import mongoose, {model} from "mongoose";
-import {LocationSchema, LocationType} from "mongoose/locations/schema";
-
-export default mongoose.models.locations ||
-    model<LocationType>("locations", LocationSchema);
