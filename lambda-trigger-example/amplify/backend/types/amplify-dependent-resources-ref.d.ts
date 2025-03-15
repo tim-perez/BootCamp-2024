8ff3f9738389501851353a698781a5ab972fcfde
Tim Perez <timperez624@gmail.com>
2025-03-15 03:13:11 -0700

    lambda-commit

---
+++ lambda-trigger-example/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "lambdatriggerexample4b5cc288": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "lambdatriggerexample4b5cc288PostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
